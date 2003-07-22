a9ae92d2df85694701e94c6d7735f7a15aea6aec
pmorch <pmorch@fc158eef-6c16-0410-b9c5-873085b46621>
2003-07-22 11:18:37 +0000

    dont include version nr. in JAVA_HOME and ANT_HOME, but rely on user softlinks instead

--- jhotdraw6/compile.sh
+++ jhotdraw6/compile.sh
@@ -2,8 +2,8 @@
 
 # Set up JAVA_HOME and ANT_HOME as environment variables, 
 # or they will use these hard-coded defaults
-export JAVA_HOME=${JAVA_HOME:-/usr/j2sdk1.4.1_01/}
-export ANT_HOME=${ANT_HOME:-/usr/apache-ant-1.5.2/}
+export JAVA_HOME=${JAVA_HOME:-/usr/j2sdk/}
+export ANT_HOME=${ANT_HOME:-/usr/apache-ant/}
 
 export PATH=$PATH:$ANT_HOME/bin
 
