89c5ee6779da5824e7c1efaa6e52fefc7f3a57fd
pmorch <pmorch@fc158eef-6c16-0410-b9c5-873085b46621>
2004-03-11 08:02:26 +0000

    * Allow Iconkit to fetch Images from URLs as well as resources. Many methods, such as getImage now have a getImageURL counterpart. I kept the original method names for BW compatibility reasons.
    * The list of images to retrieve (private List fRegisteredImages)
    should be a Set, not a list. It made no sense to allow retrieval of
    the same image twice.
    
    * The constructor takes a Component instance. In all methods that
    require a Component, use the one given in the constructor, if the
    supplied Component instance is null.
    
    * getImage should register an image if it wasn't registered before
    loadRegisteredImages(), so that registering an image is automatic.
    This is to avoid having to do this everywhere I use the Iconkit:
    Image image = Iconkit.instance().getImage(file);
    if (null == image) {
    // register the image and try again
    Iconkit.instance().registerImage(file);
    image = Iconkit.instance().getImage(file);
    }
    Now I can just:
    Image image = Iconkit.instance().getImage(file);

--- jhotdraw6/src/org/jhotdraw/util/Iconkit.java
+++ jhotdraw6/src/org/jhotdraw/util/Iconkit.java
@@ -16,7 +16,7 @@ import java.awt.*;
 import java.awt.image.ImageProducer;
 import java.net.URL;
 import java.util.Iterator;
-import java.util.List;
+import java.util.Set;
 import java.util.Map;
 import java.util.Hashtable;
 
@@ -37,12 +37,13 @@ import java.util.Hashtable;
  * @version <$CURRENT_VERSION$>
  */
 public class Iconkit {
+	/* Holds URLs as keys, and Images as values */
 	private Map                 fMap;
-	private List                fRegisteredImages;
+	/* Holds URLs */
+	private Set                 fRegisteredImages;
 	private Component           fComponent;
 	private final static int    ID = 123;
 	private static Iconkit      fgIconkit = null;
-	private static boolean      fgDebug = false;
 
 	/**
 	 * Constructs an Iconkit that uses the given editor to
@@ -50,7 +51,7 @@ public class Iconkit {
 	 */
 	public Iconkit(Component component) {
 		fMap = new Hashtable(53);
-		fRegisteredImages = CollectionsFactory.current().createList(10);
+		fRegisteredImages = CollectionsFactory.current().createSet();
 		fComponent = component;
 		fgIconkit = this;
 	}
@@ -64,20 +65,25 @@ public class Iconkit {
 
 	/**
 	 * Loads all registered images.
+	 * If component is null, the component supplied in the
+	 * constructor will be used.
 	 * @see #registerImage
 	 */
 	public void loadRegisteredImages(Component component) {
 		if (fRegisteredImages.size() == 0)
 			return;
 
-		MediaTracker tracker = new MediaTracker(component);
+		if (component == null) {
+			component = fComponent;
+		}
 
+		MediaTracker tracker = new MediaTracker(component);
 		// register images with MediaTracker
 		Iterator iter = fRegisteredImages.iterator();
 		while (iter.hasNext()) {
-			String fileName = (String)iter.next();
-			if (basicGetImage(fileName) == null) {
-				tracker.addImage(loadImage(fileName), ID);
+			URL url = (URL)iter.next();
+			if (basicGetImageURL(url) == null) {
+				tracker.addImage(loadImageURL(url), ID);
 			}
 		}
 		fRegisteredImages.clear();
@@ -92,53 +98,46 @@ public class Iconkit {
 	}
 
 	/**
-	 * Registers an image that is then loaded together with
+	 * Registers a URL that is then loaded together with
 	 * the other registered images by loadRegisteredImages.
 	 * @see #loadRegisteredImages
 	 */
+	public void registerImageURL(URL url) {
+		fRegisteredImages.add(url);
+	}
+	
+	/**
+	 * Registers the URL for the image resource
+	 * @see #registerImageURL
+	 */
 	public void registerImage(String fileName) {
-		fRegisteredImages.add(fileName);
+		registerImageURL(getResourceURL(fileName));
 	}
 
 	/**
-	 * Registers and loads an image.
+	 * Loads an image URL with the given name.
 	 */
-	public Image registerAndLoadImage(Component component, String fileName) {
-		registerImage(fileName);
-		loadRegisteredImages(component);
-		return getImage(fileName);
+	public Image loadImageURL(URL url) {
+		if (fMap.containsKey(url)) {
+			return (Image) fMap.get(url);
+		}
+		Image image = loadImageResourceURL(url);
+		if (image != null) {
+			fMap.put(url, image);
+		}
+		return image;
 	}
 
 	/**
 	 * Loads an image with the given name.
 	 */
-	public Image loadImage(String filename) {
-		if (fMap.containsKey(filename)) {
-			return (Image) fMap.get(filename);
-		}
-		Image image = loadImageResource(filename);
-		if (image != null) {
-			fMap.put(filename, image);
-		}
-		return image;
+	public Image loadImage(String fileName) {
+		return loadImageURL(getResourceURL(fileName));
 	}
 
-	public Image loadImage(String filename, boolean waitForLoad) {
-		Image image = loadImage(filename);
-		if (image!=null && waitForLoad) {
-			ImageIcon icon = new ImageIcon(image);
-			image = icon.getImage(); //this forces the wait to happen
-		}
-		return image;
-	}
-
-	public Image loadImageResource(String resourcename) {
+	public Image loadImageResourceURL(URL url) {
 		Toolkit toolkit = Toolkit.getDefaultToolkit();
 		try {
-			URL url = getClass().getResource(resourcename);
-			if (fgDebug) {
-				System.out.println(resourcename);
-			}
 			return toolkit.createImage((ImageProducer) url.getContent());
 		}
 		catch (Exception ex) {
@@ -146,26 +145,76 @@ public class Iconkit {
 		}
 	}
 
+	public Image loadImageResource(String fileName) {
+		return loadImageResourceURL(getResourceURL(fileName));
+	}
+
 	/**
-	 * Gets the image with the given name. If the image
-	 * can't be found it tries it again after loading
-	 * all the registered images.
+	 * Registers and loads an image.
+	 * If component is null, the component supplied in the
+	 * constructor will be used.
 	 */
-	public Image getImage(String filename) {
-		Image image = basicGetImage(filename);
+	public Image registerAndLoadImageURL(Component component, URL url) {
+		registerImageURL(url);
+		loadRegisteredImages(component);
+		return getImageURL(url);
+	}
+	/**
+	 * Registers and loads an image.
+	 * If component is null, the component supplied in the
+	 * constructor will be used.
+	 */
+	public Image registerAndLoadImage(Component component, String fileName) {
+		return registerAndLoadImageURL(component, getResourceURL(fileName));
+	}
+
+	public Image loadImageURL(URL url, boolean waitForLoad) {
+		Image image = loadImageURL(url);
+		if (image!=null && waitForLoad) {
+			ImageIcon icon = new ImageIcon(image);
+			image = icon.getImage(); //this forces the wait to happen
+		}
+		return image;
+	}
+
+	public Image loadImage(String fileName, boolean waitForLoad) {
+		return loadImageURL(getResourceURL(fileName), waitForLoad);
+	}
+	
+	/**
+	 * Gets the image with the given URL. If the image can't be
+	 * found it tries it again after registering the image and
+	 * loading all the registered images.
+	 */
+	public Image getImageURL(URL url) {
+		Image image = basicGetImageURL(url);
 		if (image != null) {
 			return image;
 		}
+		registerImageURL(url);
 		// load registered images and try again
 		loadRegisteredImages(fComponent);
 		// try again
-		return basicGetImage(filename);
+		return basicGetImageURL(url);
+	}
+	/**
+	 * Gets the image with the given fileName. If the image can't
+	 * be found it tries it again after registering the image and
+	 * loading all the registered images.
+	 */
+	public Image getImage(String fileName) {
+		return getImageURL(getResourceURL(fileName));
 	}
 
-	private Image basicGetImage(String filename) {
-		if (fMap.containsKey(filename)) {
-			return (Image) fMap.get(filename);
+	private URL getResourceURL(String resourceName) {
+		return getClass().getResource(resourceName);
+	}
+
+	private Image basicGetImageURL(URL url) {
+		if (fMap.containsKey(url)) {
+			return (Image) fMap.get(url);
 		}
 		return null;
 	}
+
 }
