c2af31f80b104407549cb68cf5fbbda5a7fd05bf
rawcoder <werner.randelshofer@bluewin.ch>
2007-01-14 09:30:57 +0000

    Standard Toolbar is now created by the DefaultApplicationModel instead of by each one of the DefaultApplications.

--- jhotdraw6/src/main/java/org/jhotdraw/util/StorageFormat.java
+++ jhotdraw6/src/main/java/org/jhotdraw/util/StorageFormat.java
@@ -4,7 +4,7 @@
  * Project:		JHotdraw - a GUI framework for technical drawings
  *				http://www.jhotdraw.org
  *				http://jhotdraw.sourceforge.net
- * Copyright:	© by the original author(s) and all contributors
+ * Copyright:	(c) by the original author(s) and all contributors
  * License:		Lesser GNU Public License (LGPL)
  *				http://www.opensource.org/licenses/lgpl-license.html
  */
