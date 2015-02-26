558c9cd1d15fd22c62740838cf63db1f895320d0
Tobias Warneke <t.warneke@gmx.net>
2015-02-26 17:10:54 +0100

    utils and xml into separate modules

---
+++ jhotdraw-utils/src/main/java/org/jhotdraw/util/prefs/package-info.java
@@ -0,0 +1,18 @@
+/* @(#)package-info.java
+ *
+ * Copyright (c) 1996-2010 The authors and contributors of JHotDraw.
+ * You may not use, copy or modify this file, except in compliance with the
+ * accompanying license terms.
+ *
+ * @author Werner Randelshofer
+ * @version $Id$
+*/
+
+/**
+Contains preferences classes leveraging the java.util.prefs package.
+*/
+@DefaultAnnotation(Nonnull.class)
+package org.jhotdraw.util.prefs;
+
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import javax.annotation.Nonnull;
