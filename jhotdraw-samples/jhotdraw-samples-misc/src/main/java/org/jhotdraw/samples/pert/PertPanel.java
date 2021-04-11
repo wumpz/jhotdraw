31ca9868d1429dd092e06817584c87a7fd80179a
hareb <kenza.hareb.etu@univ-lille.fr>
2021-04-11 18:53:41 +0200

    Extract the assignment out of some expressions and Edit the structure of code

--- jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/pert/PertPanel.java
+++ jhotdraw-samples/jhotdraw-samples-misc/src/main/java/org/jhotdraw/samples/pert/PertPanel.java
@@ -44,8 +44,15 @@ public class PertPanel extends JPanel {
 
     private static final long serialVersionUID = 1L;
     private UndoRedoManager undoManager;
-    private Drawing drawing;
     private DrawingEditor editor;
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JToolBar attributesToolbar;
+    private javax.swing.JToolBar creationToolbar;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane scrollPane;
+    private javax.swing.ButtonGroup toolButtonGroup;
+    private org.jhotdraw.draw.DefaultDrawingView view;
+    // End of variables declaration//GEN-END:variables
 
     /**
      * Creates new instance.
@@ -81,29 +88,38 @@ public class PertPanel extends JPanel {
         // pb.addSeparator();
         // pb.add(new ToggleGridAction(editor));
         JMenu m = new JMenu(labels.getString("view.zoomFactor.text"));
-        JRadioButtonMenuItem rbmi;
+        JRadioButtonMenuItem rbmi1 = new JRadioButtonMenuItem(new ZoomAction(editor, 0.1, null));
         ButtonGroup group = new ButtonGroup();
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 0.1, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 0.25, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 0.5, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 0.75, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 1.0, null)));
-        rbmi.setSelected(true);
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 1.25, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 1.5, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 2, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 3, null)));
-        group.add(rbmi);
-        m.add(rbmi = new JRadioButtonMenuItem(new ZoomAction(editor, 4, null)));
-        group.add(rbmi);
+        m.add(rbmi1);
+        group.add(rbmi1);
+        JRadioButtonMenuItem rbmi2 = new JRadioButtonMenuItem(new ZoomAction(editor, 0.25, null));
+        m.add(rbmi2);
+        group.add(rbmi2);
+        JRadioButtonMenuItem rbmi3 = new JRadioButtonMenuItem(new ZoomAction(editor, 0.5, null));
+        m.add(rbmi3);
+        group.add(rbmi3);
+        JRadioButtonMenuItem rbmi4 = new JRadioButtonMenuItem(new ZoomAction(editor, 0.75, null));
+        m.add(rbmi4);
+        group.add(rbmi4);
+        JRadioButtonMenuItem rbmi5 = new JRadioButtonMenuItem(new ZoomAction(editor, 1.0, null));
+        m.add(rbmi5);
+        rbmi5.setSelected(true);
+        group.add(rbmi5);
+        JRadioButtonMenuItem rbmi6 = new JRadioButtonMenuItem(new ZoomAction(editor, 1.25, null));
+        m.add(rbmi6 );
+        group.add(rbmi6);
+        JRadioButtonMenuItem rbmi7 = new JRadioButtonMenuItem(new ZoomAction(editor, 1.5, null));
+        m.add(rbmi7);
+        group.add(rbmi7);
+        JRadioButtonMenuItem rbmi8 = new JRadioButtonMenuItem(new ZoomAction(editor, 2, null));
+        m.add(rbmi8);
+        group.add(rbmi8);
+        JRadioButtonMenuItem rbmi9 = new JRadioButtonMenuItem(new ZoomAction(editor, 3, null));
+        m.add(rbmi9 = new JRadioButtonMenuItem(new ZoomAction(editor, 3, null)));
+        group.add(rbmi9);
+        JRadioButtonMenuItem rbmi10 = new JRadioButtonMenuItem(new ZoomAction(editor, 4, null));
+        m.add(rbmi10);
+        group.add(rbmi10);
         pb.add(m);
         pb.setFocusable(false);
         creationToolbar.addSeparator();
@@ -185,12 +201,5 @@ public class PertPanel extends JPanel {
         tb.addSeparator();
         ButtonFactory.addToolTo(tb, editor, new TextAreaCreationTool(new TextAreaFigure()), "edit.createTextArea", drawLabels);
     }
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JToolBar attributesToolbar;
-    private javax.swing.JToolBar creationToolbar;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane scrollPane;
-    private javax.swing.ButtonGroup toolButtonGroup;
-    private org.jhotdraw.draw.DefaultDrawingView view;
-    // End of variables declaration//GEN-END:variables
+
 }
