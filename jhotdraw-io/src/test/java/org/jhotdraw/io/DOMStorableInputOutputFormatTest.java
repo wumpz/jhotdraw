d74c7d7842fc4ede3cd4adc013591446c0f784a6
Tobias <t.warneke@gmx.net>
2023-01-19 13:42:20 +0100

    start testing dom storable

---
+++ jhotdraw-io/src/test/java/org/jhotdraw/io/DOMStorableInputOutputFormatTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2023 JHotDraw.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301  USA
+ */
+
+package org.jhotdraw.io;
+
+import org.jhotdraw.draw.io.InputFormat;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author tw
+ */
+public class DOMStorableInputOutputFormatTest {
+
+  public DOMStorableInputOutputFormatTest() {}
+
+  @BeforeAll
+  public static void setUpClass() {}
+
+  @AfterAll
+  public static void tearDownClass() {}
+
+  @BeforeEach
+  public void setUp() {}
+
+  @AfterEach
+  public void tearDown() {}
+
+    @Test
+    public void testSomeMethod() {
+        InputFormat format = new DOMStorableInputOutputFormat(new DefaultDOMDrawFigureFactory());
+    }
+}
