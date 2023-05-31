c6321215edc127661f73fdae97271316a19f396d
Tobias <t.warneke@gmx.net>
2023-05-31 14:02:45 +0200

    started MutablePath2D

--- jhotdraw-utils/src/main/java/org/jhotdraw/geom/path/MutablePath2D.java
+++ jhotdraw-utils/src/main/java/org/jhotdraw/geom/path/MutablePath2D.java
@@ -18,17 +18,151 @@
  */
 package org.jhotdraw.geom.path;
 
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
- * Path storage as a List of Nodes. To add holes, there is a list of lists.
+ * Path storage as a List of Nodes. This is simply a decorator around a real Path2D.Double.
  *
  * @author tw
  */
-public class MutablePath2D {
+public class MutablePath2D implements Shape {
 
-  public record Node(double x, double y) {}
+  protected final List<Node> nodes = new ArrayList<>();
 
-  private final List<List<Node>> nodes = new ArrayList<>(new ArrayList<>());
+  private Path2D.Double pathCache = null;
+
+  private int windingRule = PathIterator.WIND_NON_ZERO;
+
+  @Override
+  public boolean contains(double x, double y) {
+    return toPath2D().contains(x, y);
+  }
+
+  @Override
+  public boolean contains(Point2D p) {
+    return toPath2D().contains(p);
+  }
+
+  @Override
+  public boolean contains(double x, double y, double w, double h) {
+    return toPath2D().contains(x, y, w, h);
+  }
+
+  @Override
+  public boolean contains(Rectangle2D r) {
+    return toPath2D().contains(r);
+  }
+
+  @Override
+  public Rectangle getBounds() {
+    return toPath2D().getBounds();
+  }
+
+  @Override
+  public Rectangle2D getBounds2D() {
+    return toPath2D().getBounds2D();
+  }
+
+  @Override
+  public final PathIterator getPathIterator(AffineTransform at) {
+    return toPath2D().getPathIterator(at);
+  }
+
+  @Override
+  public PathIterator getPathIterator(AffineTransform at, double flatness) {
+    return toPath2D().getPathIterator(at, flatness);
+  }
+
+  @Override
+  public boolean intersects(double x, double y, double w, double h) {
+    return toPath2D().intersects(x, y, w, h);
+  }
+
+  @Override
+  public boolean intersects(Rectangle2D r) {
+    return toPath2D().intersects(r);
+  }
+
+  public void lineTo(double x, double y) {
+    nodes.add(Node.lineTo(x, y));
+    invalidatePathCache();
+  }
+
+  public void moveTo(double x, double y) {
+    if (!nodes.isEmpty() && nodes.get(nodes.size() - 1).pointType == PathIterator.SEG_MOVETO) {
+      nodes.set(nodes.size() - 1, Node.moveTo(x, y));
+    } else {
+      nodes.add(Node.moveTo(x, y));
+    }
+    invalidatePathCache();
+  }
+  
+  public void changeNode(int idx, Consumer<Node> change) {
+    change.accept(nodes.get(idx));
+    invalidatePathCache();
+  }
+  
+  public void removeNode(int idx) {
+    nodes.remove(idx);
+    invalidatePathCache();
+  }
+
+  public Path2D.Double toPath2D() {
+    if (pathCache != null) {
+      return pathCache;
+    }
+    Path2D.Double path = new Path2D.Double(windingRule);
+    for (var node : nodes) {
+      switch (node.pointType) {
+        case PathIterator.SEG_MOVETO -> path.moveTo(node.values[0], node.values[1]);
+        case PathIterator.SEG_LINETO -> path.lineTo(node.values[0], node.values[1]);
+      }
+    }
+    pathCache = path;
+    return path;
+  }
+
+  protected void invalidatePathCache() {
+    pathCache = null;
+  }
+
+  public static class Node {
+
+    final int pointType;
+    final double values[];
+
+    public static final int[] SIZE_FOR_POINTTYPE = {2, 2, 4, 6, 0};
+
+    Node(int pointType) {
+      this.pointType = pointType;
+      this.values = new double[SIZE_FOR_POINTTYPE[pointType]];
+    }
+
+    public Node withPoint(double x, double y) {
+      values[0] = x;
+      values[1] = y;
+      return this;
+    }
+    
+    public Point2D.Double getPoint() {
+      return new Point2D.Double(values[0], values[1]);
+    }
+
+    public static Node moveTo(double x, double y) {
+      return new Node(PathIterator.SEG_MOVETO).withPoint(x, y);
+    }
+
+    public static Node lineTo(double x, double y) {
+      return new Node(PathIterator.SEG_LINETO).withPoint(x, y);
+    }
+  }
 }
