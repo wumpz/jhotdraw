bf3c516f397f84b5e75349dabb08a4fc45b56350
Tobias <t.warneke@gmx.net>
2024-12-02 11:18:35 +0100

    added undoable edit via Rotation interface for RotateHandle

--- jhotdraw-core/src/main/java/org/jhotdraw/draw/handle/RotateHandle.java
+++ jhotdraw-core/src/main/java/org/jhotdraw/draw/handle/RotateHandle.java
@@ -7,9 +7,12 @@
  */
 package org.jhotdraw.draw.handle;
 
+import java.awt.Point;
 import java.awt.geom.*;
+import javax.swing.undo.AbstractUndoableEdit;
 import org.jhotdraw.draw.figure.Figure;
 import org.jhotdraw.draw.figure.Origin;
+import org.jhotdraw.draw.figure.Rotation;
 
 /**
  * A Handle to rotate a Figure. If the Figure implements Origin, this point is returned instead of the center.
@@ -38,5 +41,36 @@ public class RotateHandle extends AbstractRotateHandle {
     Rectangle2D.Double bounds = getTransformedBounds();
     Point2D.Double origin = new Point2D.Double(bounds.getCenterX(), bounds.getCenterY());
     return origin;
+  }
+
+  @Override
+  public void trackEnd(Point anchor, Point lead, int modifiersEx) {
+    if (getOwner() instanceof Rotation rotation) {
+      double startAngle = this.getStartTheta();
+      double actualAngle = rotation.getAngle();
+
+      view.getDrawing().fireUndoableEditHappened(new AbstractUndoableEdit() {
+        @Override
+        public String getPresentationName() {
+          return "Rotation";
+        }
+
+        @Override
+        public void undo() {
+          super.undo();
+          rotation.setAngle(startAngle);
+        }
+
+        @Override
+        public void redo() {
+          super.redo();
+          rotation.setAngle(actualAngle);
+        }
+      });
+      fireAreaInvalidated(getDrawingArea());
+      // location = null;
+      invalidate();
+      fireAreaInvalidated(getDrawingArea());
+    } else super.trackEnd(anchor, lead, modifiersEx);
   }
 }
